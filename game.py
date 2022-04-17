f273897d478418facc4e8ad9d37d96d359c68d38
Magomed <scrythepvp@gmail.com>
2022-04-17 18:57:08 +0200

    feat: setup game

---
+++ game.py
@@ -0,0 +1,25 @@
+import pygame
+
+
+class Game:
+    def __ini__(self):
+        pass
+
+    def run(self):
+        pass
+
+    def event_loop(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+if __name__ == '__main__':
+    pygame.init()
+    game = Game()
+    game.run()
+    pygame.quit()
